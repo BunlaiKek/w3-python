5724420e9aae830b534094f1c5960d4b4acdc4ed
Bunlai Kek <kekbunlai@gmail.com>
2023-11-19 13:26:32 +0700

    tuples

--- list.py
+++ list.py
@@ -15,7 +15,7 @@ if ("apple" in mylist):
 
 mylist[1:3] = ["blackcurrant"]
 
-mylist.append("guava")
+mylist.append("Guava")
 
 mylist.insert(1, "wood apple")
 
@@ -45,10 +45,34 @@ newList = [x for x in mylist if "a" in x]
 
 print(newList)
 
-newList.sort()
+newList.sort(key=str.lower)
 
 print(newList)
 
 newList.sort(reverse = True)
 
 print(newList)
+
+
+numbers = [100, 40, 30, 55, 51, 200, 70]
+
+def near50(n):
+    return abs(n-50)
+
+numbers.sort(key=near50)
+
+print(numbers)
+
+anotherNumbers = numbers.copy()
+
+print("another numbers", anotherNumbers)
+
+
+combinedList = newList + numbers
+
+
+print("combined", combinedList)
+
+newList.extend(numbers);
+
+print("combined 2", newList)
---
+++ tuples.py
@@ -0,0 +1,9 @@
+myTuple = ("apple", "banana", "cherry")
+
+notTuple = ("apple",)
+
+print(myTuple)
+
+print(len(myTuple))
+
+print("type", type(notTuple))
