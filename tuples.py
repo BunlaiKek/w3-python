56d79cc05da5a45d83190956ca5958718f91264c
Bunlai Kek <kekbunlai@gmail.com>
2023-11-19 17:03:08 +0700

    unpack tuple

--- tuples.py
+++ tuples.py
@@ -1,9 +1,39 @@
-myTuple = ("apple", "banana", "cherry")
+myTuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 
-notTuple = ("apple",)
+anotherTuple = tuple(("KOI", "Betta", "Gipple"))
+
+notTuple = ("apple")
 
 print(myTuple)
 
 print(len(myTuple))
 
 print("type", type(notTuple))
+
+print("another tuple", type(anotherTuple))
+
+if "apple" in myTuple:
+    print("Yes, apple is in the fruit TUPLE")
+
+listedTuple = list(anotherTuple)
+
+# To add or remove item in tuple, we must convert the tuple to list first
+
+
+listedTuple.append("Guava")
+
+listedTuple.remove("Betta")
+
+newTuple = tuple(listedTuple)
+
+partialTuple = ("WaterMelon",)
+
+
+newTuple += partialTuple
+
+
+(KOI, Betta, Gipple) = anotherTuple
+
+print("Koi", KOI)
+
+print(newTuple)
