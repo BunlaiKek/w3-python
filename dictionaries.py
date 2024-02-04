0214012d915070ecf18e5794f0b94bad9551d646
Kek Bunlai <kekbunlai@gmail.com>
2024-02-04 16:40:15 +0700

    working on dictionary

---
+++ dictionaries.py
@@ -0,0 +1,15 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+anotherCar = dict(brand="Lexus", model="LX 600", year=2024);
+
+
+
+print("car", car)
+
+print("another car", anotherCar.keys())
+
+print(anotherCar.get("year"))
