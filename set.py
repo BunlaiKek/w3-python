b64981953e3ffd1881f242fdea6bf999710db5c7
Kek Bunlai <kekbunlai@gmail.com>
2024-02-04 15:18:05 +0700

    done set

--- set.py
+++ set.py
@@ -1,35 +1,26 @@
 mySet = {"apple", "banana", "cherry"}
-
 anotherSet = set(("blueberry", "mulberry", "strawberry"))
-
 anotherSet.add("orange")
-
 mySet.update(anotherSet)
-
-
 # you can update set with set, list, tuple, or dictionary
-
-
+# this will exclude duplicate item
 mySet.update(["lemon", "cucumber"])
-
-
 mySet.discard("kakav")
-
 #mySet.remove("kakav") # course error since kakav is not present
-
 for x in mySet:
     print(x)
-
-
 setForInteraction = {"lemon", "carrot"}
-
-
 numberSet = {1, 2,3}
-
 finalSet = mySet.union(numberSet)
 
 
 mySet.intersection_update(setForInteraction)
+
+# keep all but not the duplicates
+
+allDiff = anotherSet.symmetric_difference_update(mySet)
+
+print("all diff", allDiff)
 
 print("final set", finalSet)
 
